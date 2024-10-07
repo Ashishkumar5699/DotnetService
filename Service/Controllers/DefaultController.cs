02e2a7360497396de14a3d2daf72f168c6ef67d2
Ashish Kumar <ashishsarraf7@gmail.com>
2024-10-07 16:45:39 +0530

    Default Path return

---
+++ Service/Controllers/DefaultController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using Sonaar.Controllers;
+using Sonaar.Domain.DataContexts;
+using Sonaar.Interface;
+
+namespace Sonaar.Service.APi.Controllers
+{
+    [ApiController]
+    [Route("/")]
+	public class DefaultController : ControllerBase
+    {
+        public string Index()
+        {
+            return $"It Works on {Dns.GetHostName()} ! Sonaar Service version 0.0.1";
+        }
+    }
+}
+
