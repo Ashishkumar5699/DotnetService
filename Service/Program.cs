76bc749bb20779de26d499136fc784b17e5e6304
Ashish Kumar <ashish@Ashishs-MacBook-Air.local>
2024-10-19 12:59:47 +0530

    Fixed: remove id and value from response

--- Service/Program.cs
+++ Service/Program.cs
@@ -16,11 +16,11 @@ namespace Sonaar
             // Add services to the container.
 
             //builder.Services.AddControllers();
-            builder.Services.AddControllers()
-            .AddJsonOptions(options =>
-            {
-                options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.Preserve;
-            });
+            builder.Services.AddControllers();
+            // .AddJsonOptions(options =>
+            // {
+            //     options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.Preserve;
+            // });
 
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
